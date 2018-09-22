4e773bb926092c33faf5bfce21a2db3509fd6719
mameshiva <chie.mushiga@gmail.com>
2018-09-23 01:35:46 +0900

    Revised code to pull from 'all'. Still need to convert gender labels.

--- name.py
+++ name.py
@@ -1,20 +1,27 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 
-page = requests.get("https://b-name.jp/%E8%B5%A4%E3%81%A1%E3%82%83%E3%82%93%E5%90%8D%E5%89%8D%E8%BE%9E%E5%85%B8/m/%E3%81%82/?p=1")
-soup = BeautifulSoup(page.content, 'html.parser')
+#List to store data
+genders = []
+names = []
+yomis = []
+
+page = requests.get("https://b-name.jp/%E8%B5%A4%E3%81%A1%E3%82%83%E3%82%93%E5%90%8D%E5%89%8D%E8%BE%9E%E5%85%B8/all/%E3%81%82/?p=1")
+page_html = bs4.BeautifulSoup(page.content, 'html.parser')
+
+#Extract namelist
+namelist = page_html.find(class_="namelist")
+#print(namelist)
+
+#Extract all genders
+cellgenders = namelist.find_all(class_=["icon-woman","icon-man"])
 
 #Extract all names
-cellnames = soup.find_all(class_="cell-name")
-names = [c.get_text() for c in cellnames]
+cellnames = namelist.find_all(class_="cell-name")
+name = [c.get_text() for c in cellnames]
+names.append(name)
 
 #Extract all yomis
-cellyomis = soup.find_all(class_="cell-yomi")
-yomis = [y.get_text() for y in cellyomis]
-
-import pandas as pd
-namelist = pd.DataFrame({
-    "name": names,
-    "yomi": yomis
-})
-print(namelist)
+cellyomis = namelist.find_all(class_="cell-yomi")
+yomi = [y.get_text() for y in cellyomis]
+yomis.append(yomi)
