89399a76b482439dd8f38c78c9dc4a5f12696393
mameshiva <chie.mushiga@gmail.com>
2018-09-25 22:40:35 +0900

    Changed text to Japanese.

--- name_selector.py
+++ name_selector.py
@@ -16,76 +16,79 @@ with open ('namelist_db.csv') as f:
         if row[0] == "male":
             male_list.append(row)   
 
-print("Tutorial: Here's what you can do. Type in: ")
-print("[R] to save the name to My List.")
-print("[L] to save the name in Trash List.")
-print("[M] to see your current My List.")
-print("[T] to see your current Trash List.")
-print("[E] to exit.")
+print("チュートリアル：こんなことができるよ！")
+print("[R] = 名前をマイリストに入れる")
+print("[L] = 名前をゴミ箱に入れる")
+print("[M] = マイリストを見る")
+print("[T] = ゴミ箱を見る")
+print("[E] = 終了")
 
-user_name = input("What is your name?: ")
-print("Hi " + user_name + "!")
-gender_input = input("Which name list do you want to see, female, male or all?: ")
+user_name = input("あなたの名前は？")
+print("こんにちは、" + user_name + "さん!")
+gender_input = input("赤ちゃんの性別を選んでください。女の子、男の子、両方: ")
 
-if gender_input == "female":
+if gender_input == "女の子":
     for f in female_list:
         r = random.choice(female_list)
-        print("Name: ", " ".join(r[1:3]))
-        selection_input = input("Type in a letter: ")
+        print("名前：", " ".join(r[1:3]))
+        selection_input = input("どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("My List", my_list)
+            print("マイリスト", my_list)
         if selection_input == "T":
-            print("Trash List", trash_list)
+            print("ゴミ箱", trash_list)
         if selection_input == "E":
-            print("Here's your current My List!")
-            print("My List", my_list)
+            print("またね！")
+            print("マイリスト：", my_list)
             break
-    print("Here's your current My List!")
-    print("My List", my_list)
+    print("またね！")
+    print("マイリスト", my_list)
+    print("ゴミ箱", trash_list)
 
-if gender_input == "male":
+if gender_input == "男の子":
     for f in male_list:
         r = random.choice(male_list)
-        print("Name: ", " ".join(r[1:3]))
-        selection_input = input("Type in a letter: ")
+        print("名前：", " ".join(r[1:3]))
+        selection_input = input("どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("My List", my_list)
+            print("マイリスト", my_list)
         if selection_input == "T":
-            print("Trash List", trash_list)
+            print("ゴミ箱", trash_list)
         if selection_input == "E":
-            print("Here's your current My List!")
-            print("My List", my_list)
+            print("またね！")
+            print("マイリスト", my_list)
             break
-    print("Here's your current My List!")
-    print("My List", my_list)
+    print("またね！")
+    print("マイリスト", my_list)
+    print("ゴミ箱", trash_list)
 
-if gender_input == "all":
+if gender_input == "両方":
     for f in all_list:
         r = random.choice(all_list)
-        print("Name: ", " ".join(r[1:3]))
-        selection_input = input("Type in a letter: ")
+        print("名前：", " ".join(r[1:3]))
+        selection_input = input("どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("My List", my_list)
+            print("マイリスト", my_list)
         if selection_input == "T":
-            print("Trash List", trash_list)
+            print("ゴミ箱", trash_list)
         if selection_input == "E":
-            print("Here's your current My List!")
-            print("My List", my_list)
+            print("またね！")
+            print("マイリスト", my_list)
             break
-    print("Here's your current My List!")
-    print("My List", my_list)
+    print("またね！")
+    print("マイリスト", my_list)
+    print("ゴミ箱", trash_list)
 
 #Export list to CSV
 with open('user_name.csv', 'w', newline='') as f:
