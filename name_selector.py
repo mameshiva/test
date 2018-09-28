20c4b3faf5f6ac4e58be4947af400af9a43e5e1d
mameshiva <chie.mushiga@gmail.com>
2018-09-28 22:41:28 +0900

    Added comments and while gender_input.

--- name_selector.py
+++ name_selector.py
@@ -7,6 +7,7 @@ male_list = []
 my_list = []
 trash_list = []
 
+#Open CSV.
 with open ('namelist_db.csv') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -16,6 +17,7 @@ with open ('namelist_db.csv') as f:
         if row[0] == "male":
             male_list.append(row)   
 
+#Print tutorial.
 print("チュートリアル：こんなことができるよ！")
 print("[R] = 名前をマイリストに入れる")
 print("[L] = 名前をゴミ箱に入れる")
@@ -23,74 +25,87 @@ print("[M] = マイリストを見る")
 print("[T] = ゴミ箱を見る")
 print("[E] = 終了")
 
+#Ask for user name and gender
 user_name = input("あなたの名前は？")
 print("こんにちは、" + user_name + "さん!")
-gender_input = input("赤ちゃんの性別を選んでください。女の子、男の子、両方: ")
+gender_input = input("赤ちゃんの性別を選んでください。1. 女の子、2. 男の子、3. 両方: ")
 
-if gender_input == "女の子":
+while gender_input != "1" and gender_input != "2" and gender_input != "3":
+    gender_input = input("入力を認識できませんでした。赤ちゃんの性別を選んでください。1. 女の子、2. 男の子、3. 両方: ")
+
+#If gender is female.
+if gender_input == "1":
     for f in female_list:
         r = random.choice(female_list)
         print("名前：", " ".join(r[1:3]))
         selection_input = input("どうする？")
+        if selection_input != "R" and selection_input != "L" and selection_input != "M" and selection_input != "T" and selection_input != "E": 
+            input("入力を認識できませんでした。どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("マイリスト", my_list)
+            print("マイリスト", my_list])
         if selection_input == "T":
             print("ゴミ箱", trash_list)
         if selection_input == "E":
             print("またね！")
-            print("マイリスト：", my_list)
+            print("マイリスト", my_list])
             break
     print("またね！")
-    print("マイリスト", my_list)
-    print("ゴミ箱", trash_list)
+    print("マイリスト", my_list])
+    print("ゴミ箱", trash_list])
 
-if gender_input == "男の子":
+#If gender is male.
+if gender_input == "2":
     for f in male_list:
         r = random.choice(male_list)
         print("名前：", " ".join(r[1:3]))
         selection_input = input("どうする？")
+        if selection_input != "R" and selection_input != "L" and selection_input != "M" and selection_input != "T" and selection_input != "E": 
+            input("入力を認識できませんでした。どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("マイリスト", my_list)
+            print("マイリスト", my_list])
         if selection_input == "T":
-            print("ゴミ箱", trash_list)
+            print("ゴミ箱", trash_list])
         if selection_input == "E":
             print("またね！")
-            print("マイリスト", my_list)
+            print("マイリスト", my_list])
             break
     print("またね！")
-    print("マイリスト", my_list)
-    print("ゴミ箱", trash_list)
+    print("マイリスト", my_list])
+    print("ゴミ箱", trash_list])
 
-if gender_input == "両方":
+#If gender is both.
+if gender_input == "3":
     for f in all_list:
         r = random.choice(all_list)
         print("名前：", " ".join(r[1:3]))
         selection_input = input("どうする？")
+        if selection_input != "R" and selection_input != "L" and selection_input != "M" and selection_input != "T" and selection_input != "E": 
+            input("入力を認識できませんでした。どうする？")
         if selection_input == "R":
             my_list.append(r)
         if selection_input == "L":
             trash_list.append(r)
         if selection_input == "M":
-            print("マイリスト", my_list)
+            print("マイリスト", my_list])
         if selection_input == "T":
-            print("ゴミ箱", trash_list)
+            print("ゴミ箱", trash_list])
         if selection_input == "E":
             print("またね！")
-            print("マイリスト", my_list)
+            print("マイリスト", my_list])
             break
     print("またね！")
-    print("マイリスト", my_list)
-    print("ゴミ箱", trash_list)
+    print("マイリスト", my_list])
+    print("ゴミ箱", trash_list])
 
-#Export list to CSV
+#Export list to CSV with user name.
 filename = str(user_name + ".csv")
 with open(filename, 'w', newline='') as f:
     writer = csv.writer(f)
